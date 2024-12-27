06f37b1636515677fed7cf599df3bf2367ba60f8
ttang <ttang@Mac-mini.local>
2024-12-27 14:54:11 +0800

    feat: frame sdk

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,41 +4,56 @@ import MemeList from "@/components/memes/MemeList";
 import { SortBy } from "@/services/meme/types";
 import Search from "./Search";
 
+import { useEffect, useCallback, useState } from "react";
+import sdk, { type FrameContext } from "@farcaster/frame-sdk";
+
 const capitalizeFirstLetter = (str: string) =>
-  str[0].toUpperCase() + str.slice(1);
+	str[0].toUpperCase() + str.slice(1);
 
 export default function Home() {
-  const tabs = [
-    { name: capitalizeFirstLetter(SortBy.trending), value: SortBy.trending },
-    { name: capitalizeFirstLetter(SortBy.newest), value: SortBy.newest },
-    { name: capitalizeFirstLetter(SortBy.marketCap), value: SortBy.marketCap },
-  ];
-  return (
-    <div className="w-full">
-      <div className="w-full flex gap-4 relative">
-        <Tabs defaultValue={SortBy.trending} className="flex-1">
-          <TabsList className="w-full mb-6 max-md:mb-3 max-md:justify-center max-md:flex-col-reverse">
-            <div className="flex-1 flex flex-row items-center overflow-x-auto gap-6 max-md:gap-2 max-md:w-full">
-              {tabs.map((tab) => (
-                <TabsTrigger
-                  value={tab.value}
-                  key={tab.value}
-                  className="max-md:flex-1"
-                >
-                  {tab.name}
-                </TabsTrigger>
-              ))}
-            </div>
-            <Search />
-          </TabsList>
+	const [isSDKLoaded, setIsSDKLoaded] = useState(false);
 
-          {tabs.map((tab) => (
-            <TabsContent value={tab.value} key={tab.value}>
-              <MemeList sortBy={tab.value} />
-            </TabsContent>
-          ))}
-        </Tabs>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		const load = async () => {
+			sdk.actions.ready();
+		};
+		if (sdk && !isSDKLoaded) {
+			setIsSDKLoaded(true);
+			load();
+		}
+	}, [isSDKLoaded]);
+
+	const tabs = [
+		{ name: capitalizeFirstLetter(SortBy.trending), value: SortBy.trending },
+		{ name: capitalizeFirstLetter(SortBy.newest), value: SortBy.newest },
+		{ name: capitalizeFirstLetter(SortBy.marketCap), value: SortBy.marketCap },
+	];
+	return (
+		<div className="w-full">
+			<div className="w-full flex gap-4 relative">
+				<Tabs defaultValue={SortBy.trending} className="flex-1">
+					<TabsList className="w-full mb-6 max-md:mb-3 max-md:justify-center max-md:flex-col-reverse">
+						<div className="flex-1 flex flex-row items-center overflow-x-auto gap-6 max-md:gap-2 max-md:w-full">
+							{tabs.map((tab) => (
+								<TabsTrigger
+									value={tab.value}
+									key={tab.value}
+									className="max-md:flex-1"
+								>
+									{tab.name}
+								</TabsTrigger>
+							))}
+						</div>
+						<Search />
+					</TabsList>
+
+					{tabs.map((tab) => (
+						<TabsContent value={tab.value} key={tab.value}>
+							<MemeList sortBy={tab.value} />
+						</TabsContent>
+					))}
+				</Tabs>
+			</div>
+		</div>
+	);
 }
