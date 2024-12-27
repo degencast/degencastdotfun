8a61261b2dc71d1c06508ac6ab1f6bf072264482
ttang <ttang@Mac-mini.local>
2024-12-27 12:06:06 +0800

    feat: framev2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,44 @@
 import * as React from "react";
+import type { Metadata } from "next";
 import LandingPageHome from "@/components/landing-page/LandingPageHome";
 import Home from "@/components/Home";
 import { CAST_LANDING_PAGE } from "@/constants";
 
+const appUrl = "https://degencast.ai"; //process.env.NEXT_PUBLIC_URL;
+
+const frame = {
+	version: "next",
+	imageUrl: `${appUrl}/images/switchChainGuide.png`,
+	button: {
+		title: "Launch",
+		action: {
+			type: "launch_frame",
+			name: "DegencastAI",
+			url: appUrl,
+			splashImageUrl: `${appUrl}/images/logo.png`,
+			splashBackgroundColor: "#f7f7f7",
+		},
+	},
+};
+
+export const revalidate = 300;
+
+export async function generateMetadata(): Promise<Metadata> {
+	return {
+		title: "DegencastAI",
+		openGraph: {
+			title: "DegencastAI",
+			description: "A DegencastAI app.",
+		},
+		other: {
+			"fc:frame": JSON.stringify(frame),
+		},
+	};
+}
+
 export default function HomePage() {
-  if (CAST_LANDING_PAGE) {
-    return <LandingPageHome />;
-  }
-  return <Home />;
+	if (CAST_LANDING_PAGE) {
+		return <LandingPageHome />;
+	}
+	return <Home />;
 }
