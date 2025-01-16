a2e2ad1c8d2aa64fe629ea342ed6635f07fee71a
shixuewen <friendlysxw@163.com>
2025-01-16 15:39:05 +0800

    feat: update app config

---
+++ src/components/onchain-futures/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Onchain Futures</h1>
+    </div>
+  );
+}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,6 +8,7 @@ export const WALLET_CONNECT_PROJECT_ID =
 // CAST TOKEN
 export const CAST_TOKEN_ADDRESS = process.env.NEXT_PUBLIC_CAST_TOKEN_ADDRESS;
 
-// lading page
-export const CAST_LANDING_PAGE =
-  process.env.NEXT_PUBLIC_CAST_LANDING_PAGE === "true";
+export const DEGENCAST_APP = process.env.NEXT_PUBLIC_DEGENCAST_APP || "";
+export const DEGENCAST_APP_LANDING_PAGE = DEGENCAST_APP === "landing-page";
+export const DEGENCAST_APP_ONCHAIN_FUTURES =
+  DEGENCAST_APP === "onchain-futures";
