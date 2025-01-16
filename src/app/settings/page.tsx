c6a042992d98735e62dc5972f75ab655a196f0fb
bufan <bufan@hotmail.com>
2025-01-16 16:00:30 +0800

    feat: add settings pages

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Settings() {
+  return (
+    <div className="flex flex-row justify-center items-start gap-6 max-md:flex-col max-md:gap-3">
+        Settings
+    </div>
+  );
+}
